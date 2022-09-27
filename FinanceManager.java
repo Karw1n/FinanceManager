fc830a695c1f8288f036e877fd13e814fcd52102
Karw1n <akbbalfour@gmail.com>
2022-09-27 15:16:35 +0100

    Added the file FinanceManager and Main as a test

---
+++ FinanceManager.java
@@ -0,0 +1,29 @@
+/*
+ * 
+ */
+import java.util.Scanner;
+
+public class FinanceManager{
+    
+    public FinanceManager(){
+        /*
+         * Create new file to store information
+         */
+    }
+
+    public FinanceManager(String filename){
+        /*
+         * 
+         */
+    }
+
+    // Displays menu and retrieves decicions 
+    public int menu(){
+        int input;
+        
+
+
+
+        return input;
+    }
+}
